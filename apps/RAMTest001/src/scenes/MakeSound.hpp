878b180c0533b3ea0be4d4c737f92fbccfbb602e
James Kong <fishkingsin@gmail.com>
2017-10-29 15:06:45 +0900

    Update Sound make

--- apps/RAMTest001/src/scenes/MakeSound.hpp
+++ apps/RAMTest001/src/scenes/MakeSound.hpp
@@ -7,14 +7,14 @@ public:
     void drawImGui(){
         ImGui::Checkbox("sound", &isSound);
         ImGui::Checkbox("draw line", &isDrawLine);
-        ImGui::SliderFloat("volume", &volume, 0., 1.);
+        ImGui::SliderFloat("volume", &volume, 0.0000001, 0.1);
         ImGui::Checkbox("head synth", &isHeadSynth);
         
     }
     void setup(){
         vector<ofVec3f> hl;
         hl.assign(6, ofVec3f());
-        handsline.assign(3, hl);
+//        handsline.assign(3, hl);
         
         soundline.assign(3, ofPolyline());
         
@@ -30,29 +30,30 @@ public:
         ofVec3f actor1Position;
         
         for (int j = 0; j < getNumNodeArray(); j++) {
+            vector<int> &pointsIndex = pointsIndex1;//(j%2==0) ? pointsIndex1 : pointsIndex2;
             rdtk::NodeArray actor = getNodeArray(j);
-            
-            for (int i = 0; i < pointsIndex.size(); i++) {
-                ofVec3f p = actor.getNode(pointsIndex[i]).getGlobalPosition();
-                handsline[j][i] = p;
-            }
-            
-            float least = handsline[j][0].y;
-            float most = handsline[j][0].y;
-            
-            for (int i = 1; i < pointsIndex.size(); i++) {
-                
-                float value = handsline[j][i].y;
-                if (least > value) least = value;
-                if (most < value) most = value;
-                
-            }
-            
             soundline[j].clear();
-            
             for (int i = 0; i < pointsIndex.size(); i++) {
-                
-                float val = ofMap(handsline[j][i].y, least, most, -1., 1.);
+                ofVec3f p = actor.getNode(pointsIndex[i]).getOrientationEuler();
+//                handsline[j][i] = p;
+//            }
+            
+//            float least = handsline[j][0].y;
+//            float most = handsline[j][0].y;
+//
+//            for (int i = 1; i < pointsIndex.size(); i++) {
+//
+//                float value = handsline[j][i].y;
+//                if (least > value) least = value;
+//                if (most < value) most = value;
+//
+//            }
+            
+//            soundline[j].clear();
+            
+//            for (int i = 0; i < pointsIndex.size(); i++) {
+            
+                float val = ofMap(p.y, -180, 180, -1., 1.);
                 soundline[j].curveTo(1. * i, val);
                 
             }
@@ -77,7 +78,7 @@ public:
                     index = 127 - nind;
                 }
                 if(j<soundBuffer.size()){
-                    soundBuffer[j][i] = soundline[j][index].y * volume;
+                    soundBuffer[j][i] = soundline[j][index].y ;
                 }
                 
             }
@@ -92,8 +93,8 @@ public:
                 if(j == 1 ){
                     float d1 = actor1Position.distance(p);
                     float d2 = actor1Position.distance(p);
-                    leftVolume = ofMap(d1,0,500,1.0,0,true);
-                    rightVolume = ofMap(d2,0,500,1.0,0,true);
+                    leftVolume = ofMap(d1,0,600,0.9,0.000001,true);
+                    rightVolume = ofMap(d2,0,600,0.9,0.000001,true);
                 }
             }
             if (isHeadSynth) {
@@ -141,8 +142,11 @@ public:
         for (int i = 0; i < bufferSize; i++) {
             
             for (int j = 0; j < length ; j++) {
-                output[i * nChannels] += (soundBuffer[j][i] * sin(phase[j]))*leftVolume;
-                output[i * nChannels + 1] += soundBuffer[j][i] * sin(phase[j])*rightVolume;
+                if(i%2==0){
+                    output[i * nChannels] += ((soundBuffer[j][i] * sin(phase[j]))*leftVolume)* volume;
+                }else{
+                    output[i * nChannels + 1] += (soundBuffer[j][i] * sin(phase[j])*rightVolume)* volume;
+                }
                 phase[j] += phaseAdder[j];
             }
             
@@ -151,7 +155,7 @@ public:
     string getName() const { return "MakeSound"; }
 
 private:
-    vector<int> pointsIndex = {
+    vector<int> pointsIndex1 = {
         22, //rdtk::Actor::JOINT_RIGHT_HAND,
         21, //rdtk::Actor::JOINT_RIGHT_WRIST,
         20, //rdtk::Actor::JOINT_RIGHT_ELBOW,
@@ -161,7 +165,7 @@ private:
         3 //rdtk::Actor::JOINT_NECK,
         
     };
-//    vector<int> pointsIndex = {
+//    vector<int> pointsIndex2 = {
 //        22, //rdtk::Actor::JOINT_RIGHT_HAND,
 //        21, //rdtk::Actor::JOINT_RIGHT_WRIST,
 //        20, //rdtk::Actor::JOINT_RIGHT_ELBOW,
@@ -183,15 +187,11 @@ private:
         rdtk::Actor::JOINT_ABDOMEN, //rdtk::Actor::JOINT_RIGHT_SHOULDER,
         rdtk::Actor::JOINT_HIPS, //rdtk::Actor::JOINT_RIGHT_COLLAR,
         
-        rdtk::Actor::JOINT_LEFT_HIP, //rdtk::Actor::JOINT_NECK,
-        
-        rdtk::Actor::JOINT_HIPS, //rdtk::Actor::JOINT_LEFT_COLLAR,
-        rdtk::Actor::JOINT_ABDOMEN, //rdtk::Actor::JOINT_LEFT_SHOULDER,
-        rdtk::Actor::JOINT_CHEST, //rdtk::Actor::JOINT_LEFT_ELBOW,
-        rdtk::Actor::JOINT_NECK, //rdtk::Actor::JOINT_LEFT_WRIST,
-        rdtk::Actor::JOINT_HEAD  //rdtk::Actor::JOINT_LEFT_HAND
+        rdtk::Actor::JOINT_LEFT_HIP
     };
-    vector<vector<ofVec3f>> handsline;
+    
+    
+//    vector<vector<ofVec3f>> handsline;
     vector<ofPolyline> soundline;
     vector<vector<float>> soundBuffer;
     vector<float> frequency;
