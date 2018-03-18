f7b72638ed7938e22c3aefaf7c921b28825cbb5c
James Kong <fishkingsin@gmail.com>
2018-03-18 23:43:13 +0800

    Flickr2

--- apps/RAMTest001/src/scenes/FlickerControl2/FlickerControl2.h
+++ apps/RAMTest001/src/scenes/FlickerControl2/FlickerControl2.h
@@ -17,7 +17,11 @@ class FlickerControl2 : public rdtk::BaseScene
 public:
     
     //--------------------------------------------------------------
-    FlickerControl2() {
+    FlickerControl2() :
+    constantTrigger(false),
+    fadeOut(0.1),
+    isCoolDownConstant(100),
+    minAcc(0.5){
         
         name = "Flicker Control2";
         
@@ -26,7 +30,18 @@ public:
         midiOut.listPorts();
         midiOut.openPort(0);
         
+        accumAcceleration.clear();
+        accumAcceleration.resize(rdtk::Actor::NUM_JOINTS);
+        std::fill(accumAcceleration.begin(), accumAcceleration.end(), 0);
         
+        isTriggered.clear();
+        isTriggered.resize(rdtk::Actor::NUM_JOINTS);
+        std::fill(isTriggered.begin(), isTriggered.end(), false);
+        for (int i=0; i<rdtk::Actor::NUM_JOINTS; i++)
+        {
+            
+            mNodeVisibility[i] = false;
+        }
     }
     
     //--------------------------------------------------------------
@@ -44,227 +59,173 @@ public:
     //--------------------------------------------------------------
     void drawImGui()
     {
-//        ImGui::Checkbox("isDebug", &isDebug);
+        //        ImGui::Checkbox("isDebug", &isDebug);
         ImGui::Checkbox("Constant Trigger", &constantTrigger);
         ImGui::SliderFloat("Low Pass", &lowpassamount,0.0,1.0);
         ImGui::SliderInt("Velocity", &iVelocity,0,127);
-        
-        
+        ImGui::SliderFloat("fadeOut", &fadeOut,0.0,1.0);
+        ImGui::SliderFloat("minAcc", &minAcc,0.0,1.0);
+        ImGui::SliderInt("isCoolDownConstant", &isCoolDownConstant,1,1000);
+        if (ImGui::Button("Set All")) {
+            for (int i=0; i<rdtk::Actor::NUM_JOINTS; i++)
+            {
+                mNodeVisibility[i] = true;
+            }
+        }
+        if (ImGui::Button("Unset All")) {
+            for (int i=0; i<rdtk::Actor::NUM_JOINTS; i++)
+            {
+                mNodeVisibility[i] = false;
+            }
+        }
+        ImGui::Separator();
+        ImGui::Columns(3, NULL, true);
+        for (int i=0; i<rdtk::Actor::NUM_JOINTS; i++)
+        {
+            ImGui::Checkbox(rdtk::Actor::getJointName(i).c_str(), &mNodeVisibility[i]);
+            ImGui::NextColumn();
+        }
+        ImGui::Columns(1, NULL, true);
+        ImGui::Separator();
+        for (int i=0; i<rdtk::Actor::NUM_JOINTS; i++)
+        {
+            if(mNodeVisibility[i]){
+                
+                //                ImGui::Columns(1, NULL, true);
+                //                ImGui::PushID(ofToString(i).c_str());
+                //                ImGui::Text("%s value",rdtk::Actor::getJointName(i).c_str());
+                ImGui::DragFloat((rdtk::Actor::getJointName(i)+" acc").c_str(), &accumAcceleration[i],0.01,0,1);
+                
+                //
+                //
+            }
+        }
+        ImGui::Columns(1, NULL, true);
     }
     
     //--------------------------------------------------------------
     void drawActor(const rdtk::Actor &actor){
         
-        const rdtk::NodeArray actor1 = lowPassFilter.update(actor);
-//        if(isDebug){
-//            rdtk::DrawBasicActor(actor1);
-//
-//            ofDrawBitmapString("Left Elbow: " + ofToString(actor.getNode(rdtk::Actor::JOINT_LEFT_ELBOW).getOrientationEuler()) ,10,300);
-//            ofDrawBitmapString("Right Elbow: "+ ofToString(actor.getNode(rdtk::Actor::JOINT_RIGHT_ELBOW).getOrientationEuler()) ,10,315);
-//            ofDrawBitmapString("Left Knee: "+ ofToString(actor.getNode(rdtk::Actor::JOINT_LEFT_KNEE).getOrientationEuler()) ,10,330);
-//            ofDrawBitmapString("Right Knee: "+ ofToString(actor.getNode(rdtk::Actor::JOINT_RIGHT_KNEE).getOrientationEuler()) ,10,345);
-//        }
-//        int leftE = 0;
-//        switch(activeOrientation[0])
-//        {
-//            case 0:
-//            {
-//                leftE = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_ELBOW).getOrientationEuler().x,
-//                              rotationRangeLeftElbowX[0],rotationRangeLeftElbowX[1],
-//                              0,9);
-//            }
-//                break;
-//            case 1:
-//            {
-//                leftE = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_ELBOW).getOrientationEuler().y,
-//                              rotationRangeLeftElbowY[0],rotationRangeLeftElbowY[1],
-//                              0,9);
-//            }
-//                break;
-//            case 2:
-//            {
-//                leftE = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_ELBOW).getOrientationEuler().z,
-//                              rotationRangeLeftElbowZ[0],rotationRangeLeftElbowZ[1],
-//                              0,9);
-//            }
-//                break;
-//            default: break;
-//        }
-//
-//        int rightE = 0;
-//        switch(activeOrientation[1])
-//        {
-//            case 0:
-//            {
-//                rightE = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_ELBOW).getOrientationEuler().x,
-//                               rotationRangeRightElbowX[0],rotationRangeRightElbowX[1],
-//                               10,19);
-//            }
-//                break;
-//            case 1:
-//            {
-//                rightE = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_ELBOW).getOrientationEuler().y,
-//                               rotationRangeRightElbowY[0],rotationRangeRightElbowY[1],
-//                               10,19);
-//            }
-//                break;
-//            case 2:
-//            {
-//                rightE = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_ELBOW).getOrientationEuler().z,
-//                               rotationRangeRightElbowZ[0],rotationRangeRightElbowZ[1],
-//                               10,19);
-//            }
-//                break;
-//            default:
-//                break;
-//        }
-//
-//        int leftK = 0;
-//        switch(activeOrientation[2])
-//        {
-//            case 0:
-//            {
-//                leftK = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_KNEE).getOrientationEuler().x,
-//                              rotationRangeLeftKneeX[0],rotationRangeLeftKneeX[1],
-//                              20,29);
-//            }
-//                break;
-//            case 1:
-//            {
-//                leftK = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_KNEE).getOrientationEuler().y,
-//                              rotationRangeLeftKneeY[0],rotationRangeLeftKneeY[1],
-//                              20,29);
-//            }
-//                break;
-//            case 2:
-//            {
-//                leftK = ofMap(actor.getNode(rdtk::Actor::JOINT_LEFT_KNEE).getOrientationEuler().z,
-//                              rotationRangeLeftKneeZ[0],rotationRangeLeftKneeZ[1],
-//                              20,29);
-//            }
-//                break;
-//            default: break;
-//        }
-//
-//        int rightK = 0;
-//        switch(activeOrientation[3])
-//        {
-//            case 0:
-//            {
-//                rightK = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_KNEE).getOrientationEuler().x,
-//                              rotationRangeRightKneeX[0],rotationRangeRightKneeX[1],
-//                              30,39);
-//            }
-//                break;
-//            case 1:
-//            {
-//                rightK = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_KNEE).getOrientationEuler().y,
-//                              rotationRangeRightKneeY[0],rotationRangeRightKneeY[1],
-//                              30,39);
-//            }
-//                break;
-//            case 2:
-//            {
-//                rightK = ofMap(actor.getNode(rdtk::Actor::JOINT_RIGHT_KNEE).getOrientationEuler().z,
-//                              rotationRangeRightKneeZ[0],rotationRangeRightElbowZ[1],
-//                              30,39);
-//            }
-//                break;
-//            default: break;
-//        }
-//
-//        currentTrackId[0] = ofClamp(leftE,currentTrackIdRange1[0],currentTrackIdRange1[1]);//ofClamp(leftE,0,9);
-//        currentTrackId[1] = ofClamp(rightE,currentTrackIdRange2[0],currentTrackIdRange2[1]);
-//        currentTrackId[2] = ofClamp(leftK,currentTrackIdRange3[0],currentTrackIdRange3[1]);
-//        currentTrackId[3] = ofClamp(rightK,currentTrackIdRange4[0],currentTrackIdRange4[1]);
-//
-//        if(constantTrigger) {
-//            if(currentTrackId[0] != previousTrackId[0]) {
-//                midiOut.sendNoteOn(1, currentTrackId[0],iVelocity);
-//                previousTrackId[0] = currentTrackId[0];
-//            }
-//
-//            if(currentTrackId[1] != previousTrackId[1]) {
-//                midiOut.sendNoteOn(2, currentTrackId[1],iVelocity);
-//                previousTrackId[1] = currentTrackId[1];
-//            }
-//
-//            if(currentTrackId[2] != previousTrackId[2]) {
-//                midiOut.sendNoteOn(3, currentTrackId[2],iVelocity);
-//                previousTrackId[2] = currentTrackId[2];
-//            }
-//
-//            if(currentTrackId[3] != previousTrackId[3]) {
-//                midiOut.sendNoteOn(4, currentTrackId[3],iVelocity);
-//                previousTrackId[3] = currentTrackId[3];
-//            }
-//
-//            usleep(5);
-//            midiOut.sendNoteOn(1,currentTrackId[0],0);
-//            midiOut.sendNoteOn(2,currentTrackId[1],0);
-//            midiOut.sendNoteOn(3,currentTrackId[2],0);
-//            midiOut.sendNoteOn(4,currentTrackId[3],0);
-//
-//            float back = ofMap(actor.getNode(rdtk::Actor::JOINT_CHEST).getOrientationEuler().x,-180,180,10,127);
-//            back = ofClamp(back,10,127);
-//            midiOut.sendControlChange(2,10,back);
-//        }
+        //        const rdtk::NodeArray actor1 = lowPassFilter.update(actor);
+        for(int nodeID = 0 ; nodeID < rdtk::Actor::NUM_JOINTS ; nodeID++){
+            const rdtk::Node &node = actor.getNode(nodeID);
+            if(node.getAcceleration().length()>minAcc){
+            
+                accumAcceleration[nodeID] += abs(node.getAcceleration().length());
+                
+            
+                
+            }
+        }
+        //        if(constantTrigger) {
+        //            if(currentTrackId[0] != previousTrackId[0]) {
+        //                midiOut.sendNoteOn(1, currentTrackId[0],iVelocity);
+        //                previousTrackId[0] = currentTrackId[0];
+        //            }
+        //
+        //            if(currentTrackId[1] != previousTrackId[1]) {
+        //                midiOut.sendNoteOn(2, currentTrackId[1],iVelocity);
+        //                previousTrackId[1] = currentTrackId[1];
+        //            }
+        //
+        //            if(currentTrackId[2] != previousTrackId[2]) {
+        //                midiOut.sendNoteOn(3, currentTrackId[2],iVelocity);
+        //                previousTrackId[2] = currentTrackId[2];
+        //            }
+        //
+        //            if(currentTrackId[3] != previousTrackId[3]) {
+        //                midiOut.sendNoteOn(4, currentTrackId[3],iVelocity);
+        //                previousTrackId[3] = currentTrackId[3];
+        //            }
+        //
+        //            usleep(5);
+        //            midiOut.sendNoteOn(1,currentTrackId[0],0);
+        //            midiOut.sendNoteOn(2,currentTrackId[1],0);
+        //            midiOut.sendNoteOn(3,currentTrackId[2],0);
+        //            midiOut.sendNoteOn(4,currentTrackId[3],0);
+        //
+        //            float back = ofMap(actor.getNode(rdtk::Actor::JOINT_CHEST).getOrientationEuler().x,-180,180,10,127);
+        //            back = ofClamp(back,10,127);
+        //            midiOut.sendControlChange(2,10,back);
+        //        }
     }
     
     void draw()
     {
-//        if(isDebug){
-//            ofDrawBitmapString("Left Elbow Active Orientation: " + ofToString(activeOrientation[0]) ,10,100);
-//            ofDrawBitmapString("Right Elbow Active Orientation: "+ ofToString(activeOrientation[1]) ,10,115);
-//            ofDrawBitmapString("Left Knee Active Orientation: "+ ofToString(activeOrientation[2]) ,10,130);
-//            ofDrawBitmapString("Right Knee Active Orientation: "+ ofToString(activeOrientation[3]) ,10,145);
-//
-//            ofDrawBitmapString("Left Elbow Track ID: " + ofToString(currentTrackId[0]) ,10,200);
-//            ofDrawBitmapString("Right Elbow Track ID: "+ ofToString(currentTrackId[1]) ,10,215);
-//            ofDrawBitmapString("Left Knee Track ID: "+ ofToString(currentTrackId[2]) ,10,230);
-//            ofDrawBitmapString("Right Knee Track ID: "+ ofToString(currentTrackId[3]) ,10,245);
-//        }
+    }
+    void update(){
+        for(int nodeID = 0 ; nodeID < rdtk::Actor::NUM_JOINTS ; nodeID++){
+            int midiNodeID = nodeID;
+            if(mNodeVisibility[nodeID] && constantTrigger){
+                
+                if(accumAcceleration[nodeID]>1){
+                    if(!isTriggered[nodeID]){
+                        ofLogVerbose() << "nodeID%4 " << nodeID%4 << " midiNodeID " << midiNodeID;
+                        midiOut.sendNoteOn(nodeID%4,midiNodeID,iVelocity);
+                        isTriggered[nodeID] = true;
+                        isCoolDown[nodeID] = isCoolDownConstant;
+                    }
+                }
+//                else if(accumAcceleration[nodeID].y>1){
+//                    if(!isTriggered[midiNodeID+1]){
+//                        ofLogVerbose() << "nodeID%4 " << nodeID%4 << " midiNodeID " << midiNodeID+1;
+//                        midiOut.sendNoteOn(nodeID%4,midiNodeID+1,iVelocity);
+//                        isTriggered[midiNodeID+1] = true;
+//                        isCoolDown[nodeID] = isCoolDownConstant;
+//                    }
+//                }
+//                else if(accumAcceleration[nodeID].z>1){
+//                    if(!isTriggered[midiNodeID+2]){
+//                        ofLogVerbose() << "nodeID%4 " << nodeID%4 << " midiNodeID " << midiNodeID+2;
+//                        midiOut.sendNoteOn(nodeID%4,midiNodeID+2,iVelocity);
+//                        isTriggered[midiNodeID+2] = true;
+//                        isCoolDown[nodeID] = isCoolDownConstant;
+//                    }
+//                }
+            }
+            isCoolDown[nodeID] = ofLerp(isCoolDown[nodeID], 0, fadeOut);
+            if(isTriggered[nodeID] && accumAcceleration[nodeID] < 1 && isCoolDown[nodeID]<1){
+                isTriggered[nodeID] = false;
+                midiOut.sendNoteOff(nodeID%4,midiNodeID,0);
+            }
+//            if(isTriggered[midiNodeID+1] && accumAcceleration[nodeID].y < 1 && isCoolDown[nodeID]<1){
+//                isTriggered[midiNodeID+1] = false;
+//                midiOut.sendNoteOff(nodeID%4,midiNodeID+1,0);
+//            }
+//            if(isTriggered[midiNodeID+2] && accumAcceleration[nodeID].z < 1 && isCoolDown[nodeID]<1){
+//                isTriggered[midiNodeID+2] = false;
+//                midiOut.sendNoteOff(nodeID%4,midiNodeID+2,0);
+//            }
+            accumAcceleration[nodeID] = ofLerp(accumAcceleration[nodeID], 0, fadeOut);
+//            accumAcceleration[nodeID].y = ofLerp(accumAcceleration[nodeID].y, 0, fadeOut);
+//            accumAcceleration[nodeID].z = ofLerp(accumAcceleration[nodeID].z, 0, fadeOut);
+            
+            
+        }
     }
     
+    
     ofxMidiOut midiOut;
-//    bool isDebug;
-//    float startAngle;
-//    float endAngle;
-//    float speedWhichRotated;
-//    
+    //    bool isDebug;
+    //    float startAngle;
+    //    float endAngle;
+    //    float speedWhichRotated;
+    //
     int iVelocity;
-//
-//    int activeOrientation[4];
-//    int currentTrackId[4];
-//    int previousTrackId[4];
-//    
-//    int rotationRangeLeftElbowX[2];
-//    int rotationRangeLeftElbowY[2];
-//    int rotationRangeLeftElbowZ[2];
-//    
-//    int rotationRangeRightElbowX[2];
-//    int rotationRangeRightElbowY[2];
-//    int rotationRangeRightElbowZ[2];
-//    
-//    int rotationRangeLeftKneeX[2];
-//    int rotationRangeLeftKneeY[2];
-//    int rotationRangeLeftKneeZ[2];
-//    
-//    int rotationRangeRightKneeX[2];
-//    int rotationRangeRightKneeY[2];
-//    int rotationRangeRightKneeZ[2];
-//    
-//    int currentTrackIdRange1[2];
-//    int currentTrackIdRange2[2];
-//    int currentTrackIdRange3[2];
-//    int currentTrackIdRange4[2];
     
     
     rdtk::LowPassFilter lowPassFilter;
     float lowpassamount;
     bool constantTrigger;
-    
+    float fadeOut;
     string name;
+    
+    bool mNodeVisibility[rdtk::Actor::NUM_JOINTS];
+    float isCoolDown[rdtk::Actor::NUM_JOINTS];
+    int isCoolDownConstant;
+    vector<bool> isTriggered;
+    vector<float> accumAcceleration;
+    float minAcc;
     
 };
 
